eb243d13152b56f9db55bf8c7b31dafd64248ff5
ebagabee <alvesantos.gh@gmail.com>
2026-06-10 09:25:45 -0300

    chore: update @ to src/components and update import to cn

--- src/components/steps/StepOne.tsx
+++ src/components/steps/StepOne.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+
 function StepOne() {
-    return (
-        <div>First Step</div>
-    )
+  const [destination, setDestination] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  return (
+    <Card className="w-full max-w-md">
+        <CardHeader>
+            <CardTitle>Dados da Viagem</CardTitle>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4">
+            
+        </CardContent>
+    </Card>
+  );
 }
 
 export default StepOne;
